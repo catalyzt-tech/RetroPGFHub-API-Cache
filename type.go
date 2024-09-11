8caf92fc7313e969914823ba04521d08fbab7864
tgrziminiar <mixzazazaza1212@gmail.com>
2024-09-11 23:01:57 +0700

    refactor: change data type to any since the data field always change

--- type.go
+++ type.go
@@ -1,5 +1,7 @@
 package main
 
+type Project interface{}
+
 type Response struct {
 	Meta Metadata  `json:"meta"`
 	Data []Project `json:"data"`
@@ -11,91 +13,143 @@ type Metadata struct {
 	NextOffset    int64 `json:"next_offset"`
 }
 
-type Project struct {
-	ID                   string              `json:"id"`
-	Category             Category            `json:"category"`
-	Name                 string              `json:"name"`
-	Description          string              `json:"description"`
-	ProfileAvatarURL     string              `json:"profileAvatarUrl"`
-	ProjectCoverImageURL string              `json:"projectCoverImageUrl"`
-	SocialLinks          SocialLinks         `json:"socialLinks"`
-	Team                 []string            `json:"team"`
-	Github               []map[string]Github `json:"github"`
-	Packages             []interface{}       `json:"packages"`
-	Contracts            []Contract          `json:"contracts"`
-	GrantsAndFunding     GrantsAndFunding    `json:"grantsAndFunding"`
-	Organization         *Organization       `json:"organization"`
-	Links                []string            `json:"links"`
-}
+// NOTE: since it always update so just use the type any for now is fine
+// type Project struct {
+// 	ID                   string           `json:"id"`
+// 	ApplicationID        string           `json:"applicationId"`
+// 	ProjectID            string           `json:"projectId"`
+// 	Category             string           `json:"category"`
+// 	ApplicationCategory  Category         `json:"applicationCategory"`
+// 	Organization         *Organization    `json:"organization"`
+// 	Name                 string           `json:"name"`
+// 	Description          string           `json:"description"`
+// 	ProfileAvatarURL     string           `json:"profileAvatarUrl"`
+// 	ProjectCoverImageURL string           `json:"projectCoverImageUrl"`
+// 	SocialLinks          SocialLinks      `json:"socialLinks"`
+// 	Team                 []Team           `json:"team"`
+// 	Github               []Github         `json:"github"`
+// 	Packages             []interface{}    `json:"packages"`
+// 	Links                []Github         `json:"links"`
+// 	Contracts            []Contract       `json:"contracts"`
+// 	GrantsAndFunding     GrantsAndFunding `json:"grantsAndFunding"`
+// 	PricingModel         PricingModel     `json:"pricingModel"`
+// 	ImpactStatement      ImpactStatement  `json:"impactStatement"`
+// }
 
-type Contract struct {
-	Address          string `json:"address"`
-	DeploymentTxHash string `json:"deploymentTxHash"`
-	DeployerAddress  string `json:"deployerAddress"`
-	ChainID          int64  `json:"chainId"`
-}
+// type Contract struct {
+// 	Address           string `json:"address"`
+// 	DeploymentTxHash  string `json:"deploymentTxHash"`
+// 	DeployerAddress   string `json:"deployerAddress"`
+// 	VerificationProof string `json:"verificationProof"`
+// 	ChainID           int64  `json:"chainId"`
+// }
 
-type Github struct {
-	RepoRank                         int64   `json:"repo_rank"`
-	StarCount                        int64   `json:"star_count"`
-	StarredEvents                    int64   `json:"starred_events"`
-	StarredByTopDevs                 int64   `json:"starred_by_top_devs"`
-	ForkCount                        int64   `json:"fork_count"`
-	ForkedEvents                     int64   `json:"forked_events"`
-	ForkedByTopDevs                  int64   `json:"forked_by_top_devs"`
-	FulltimeDeveloperAverage6_Months float64 `json:"fulltime_developer_average_6_months"`
-	NewContributorCount6_Months      int64   `json:"new_contributor_count_6_months"`
-	AgeOfProjectYears                float64 `json:"age_of_project_years"`
-}
+// type Github struct {
+// 	URL         string  `json:"url"`
+// 	Name        *string `json:"name"`
+// 	Description *string `json:"description"`
+// }
 
-type GrantsAndFunding struct {
-	VentureFunding []VentureFunding `json:"ventureFunding"`
-	Grants         []Grant          `json:"grants"`
-	Revenue        []Revenue        `json:"revenue"`
-}
+// type GrantsAndFunding struct {
+// 	VentureFunding []interface{} `json:"ventureFunding"`
+// 	Grants         []Grant       `json:"grants"`
+// 	Revenue        []interface{} `json:"revenue"`
+// }
 
-type Grant struct {
-	Grant   string `json:"grant"`
-	Link    string `json:"link"`
-	Amount  string `json:"amount"`
-	Date    string `json:"date"`
-	Details string `json:"details"`
-}
+// type Grant struct {
+// 	Grant   *string `json:"grant"`
+// 	Link    *string `json:"link"`
+// 	Amount  string  `json:"amount"`
+// 	Date    string  `json:"date"`
+// 	Details *string `json:"details"`
+// }
 
-type Revenue struct {
-	Amount  string `json:"amount"`
-	Details string `json:"details"`
-}
+// type ImpactStatement struct {
+// 	Category    Category    `json:"category"`
+// 	Subcategory []string    `json:"subcategory"`
+// 	Statement   []Statement `json:"statement"`
+// }
 
-type VentureFunding struct {
-	Amount  string `json:"amount"`
-	Year    string `json:"year"`
-	Details string `json:"details"`
-}
+// type Statement struct {
+// 	Answer   string `json:"answer"`
+// 	Question string `json:"question"`
+// }
 
-type Organization struct {
-	Name             string `json:"name"`
-	ProfileAvatarURL string `json:"profileAvatarUrl"`
-}
+// type Organization struct {
+// 	Name                      string      `json:"name"`
+// 	Description               string      `json:"description"`
+// 	OrganizationAvatarURL     string      `json:"organizationAvatarUrl"`
+// 	OrganizationCoverImageURL *string     `json:"organizationCoverImageUrl"`
+// 	SocialLinks               SocialLinks `json:"socialLinks"`
+// 	Team                      []string    `json:"team"`
+// }
 
-type SocialLinks struct {
-	Twitter   string      `json:"twitter"`
-	Farcaster []string    `json:"farcaster"`
-	Mirror    interface{} `json:"mirror"`
-	Website   []string    `json:"website"`
-}
+// type SocialLinks struct {
+// 	Website   []string `json:"website"`
+// 	Farcaster []string `json:"farcaster"`
+// 	Twitter   *string  `json:"twitter"`
+// 	Mirror    *string  `json:"mirror"`
+// }
 
-type Meta struct {
-	HasNext       bool  `json:"has_next"`
-	TotalReturned int64 `json:"total_returned"`
-	NextOffset    int64 `json:"next_offset"`
-}
+// type Team struct {
+// 	Fid               int64             `json:"fid"`
+// 	Object            Object            `json:"object"`
+// 	PfpURL            string            `json:"pfp_url"`
+// 	Profile           Profile           `json:"profile"`
+// 	Username          string            `json:"username"`
+// 	PowerBadge        bool              `json:"power_badge"`
+// 	DisplayName       string            `json:"display_name"`
+// 	ActiveStatus      ActiveStatus      `json:"active_status"`
+// 	Verifications     []string          `json:"verifications"`
+// 	FollowerCount     int64             `json:"follower_count"`
+// 	CustodyAddress    string            `json:"custody_address"`
+// 	FollowingCount    int64             `json:"following_count"`
+// 	VerifiedAddresses VerifiedAddresses `json:"verified_addresses"`
+// }
 
-type Category string
+// type Profile struct {
+// 	Bio Bio `json:"bio"`
+// }
 
-const (
-	EthereumCoreContributions     Category = "ETHEREUM_CORE_CONTRIBUTIONS"
-	OpStackReseachAndDevelopment  Category = "OP_STACK_RESEACH_AND_DEVELOPMENT"
-	OpStackResearchAndDevelopment Category = "OP_STACK_RESEARCH_AND_DEVELOPMENT"
-	OpStackTooling                Category = "OP_STACK_TOOLING"
-)
+// type Bio struct {
+// 	Text string `json:"text"`
+// }
+
+// type VerifiedAddresses struct {
+// 	EthAddresses []string `json:"eth_addresses"`
+// 	SolAddresses []string `json:"sol_addresses"`
+// }
+
+// type Meta struct {
+// 	HasNext       bool  `json:"has_next"`
+// 	TotalReturned int64 `json:"total_returned"`
+// 	NextOffset    int64 `json:"next_offset"`
+// }
+
+// type Category string
+
+// const (
+// 	EthereumCoreContributions     Category = "ETHEREUM_CORE_CONTRIBUTIONS"
+// 	OpStackResearchAndDevelopment Category = "OP_STACK_RESEARCH_AND_DEVELOPMENT"
+// 	OpStackTooling                Category = "OP_STACK_TOOLING"
+// )
+
+// type PricingModel string
+
+// const (
+// 	Free     PricingModel = "free"
+// 	Freemium PricingModel = "freemium"
+// 	PayToUse PricingModel = "pay_to_use"
+// )
+
+// type ActiveStatus string
+
+// const (
+// 	Inactive ActiveStatus = "inactive"
+// )
+
+// type Object string
+
+// const (
+// 	User Object = "user"
+// )
